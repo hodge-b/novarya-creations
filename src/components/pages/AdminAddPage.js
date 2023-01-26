9fc596721126fbbe2df4830921366db10d094e08
Brad Hodge <bradley_hodge@hotmail.com>
2023-01-25 22:06:25 -0500

    added routing to admin dashboard buttons

---
+++ src/components/pages/AdminAddPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function AdminAddPage() {
+    return(
+        <>
+            <h2>Admin Add Page</h2>
+        </>
+    )
+}
---
+++ src/components/pages/AdminManageInventory.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function AdminManageInventory() {
+    return(
+        <>
+            <h2>Admin Manage Inventory</h2>
+        </>
+    )
+}
